7b2c04061c0a4c3ba34de35dc457e9ff979d3369
S. M. Rahat Rahman <66276422+rahat44@users.noreply.github.com>
2020-07-08 00:42:48 +0600

    Update and rename untitled5.py to Split 1.py

--- Split 1.py
+++ Split 1.py
@@ -6,7 +6,7 @@ Created on Fri May 15 16:24:32 2020
 @author: smrahman
 """
 
-# 5. Provided is a list of data about a store’s inventory where each item in the list represents the name of an item, how much is in stock, and how much it costs.
+# Question: Provided is a list of data about a store’s inventory where each item in the list represents the name of an item, how much is in stock, and how much it costs.
 # Print out each item in the list with the same formatting, using the .format method (not string concatenation).
 # For example, the first print statment should read The store has 12 shoes, each for 29.99 USD.
 
